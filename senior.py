08a61e6b257e4756c31c3cbe214ff2d393c2ee4b
Yamuna2812 <hyamuna2807@gmail.com>
2026-04-17 21:38:49 +0530

    Add files via upload
    small python programe

---
+++ senior.py
@@ -0,0 +1,7 @@
+name=input("enter the name of the person")
+Birth=int(input("enter the birth year of the person"))
+age= 2026-Birth
+if(age>60):
+    print("the person is senior citizen")
+else:
+    print("the person is not senior citizen")    
